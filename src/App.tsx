ff84072e14d431c14b1aef0b561ffc7976299bb4
Joan Barruel <j.barruel@icloud.com>
2026-03-11 12:43:54 +0100

    init : setup vite reacat ts tailwind

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Services from "./components/Services"
+import Contact from "./components/Contact"
+import Localisation from "./components/Localisation"
+
+
+
+function App() {
+ 
+
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Contact />
+      <Localisation />
+      <Footer />
+    </main>
+
+    
+  )
+}
+
+export default App
---
+++ src/components/Localisation.tsx
@@ -0,0 +1,7 @@
+export default function Localisation () {
+    return (
+        <div>Localisation</div>
+    )
+
+    
+}
